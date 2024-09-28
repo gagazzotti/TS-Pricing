d4195ee36bc839c0206192d4552d42242a56e8bd
GaetanoCodes <gaetano.agazzotti@hotmail.fr>
2024-09-28 20:07:37 +0200

    convergence plot done

--- coefs_ts_series.py
+++ coefs_ts_series.py
@@ -1,10 +1,17 @@
+"""Importing modules"""
+
+import itertools as it
 import numpy as np
-import itertools as iter
+import numpy.typing as npt
 
 from scipy.special import gamma, factorial, poch
 
 
 class TemperedStablePricer:
+    """
+    Tempered Stable Pricer Pricer
+    """
+
     def __init__(
         self,
         alpha_p: float,
@@ -29,19 +36,43 @@ class TemperedStablePricer:
         return
 
     def display_params(self, k: float):
+        """Display params in the terminal
+
+        Args:
+            k (float): moneyness
+        """
         print(f"Moneyness (k): {k}")
         print(f"zeta: {self.zeta}")
         print(f"gamma: {self.gamma}")
 
-    def a(self, alpha: float, beta: float):
+    def a(self, alpha: float, beta: float) -> float:
+        """a_pm constant in the paper
+
+        Args:
+            alpha (float): alpha
+            beta (float): beta
+
+        Returns:
+            float: apm
+        """
         return -alpha * gamma(-beta)
 
-    def gamma_(self):
+    def gamma_(self) -> float:
+        """gamma constant in the paper
+
+        Returns:
+            float: gamma
+        """
         return (
             self.ap * self.lambda_p**self.beta_p + self.am * self.lambda_m**self.beta_m
         )
 
     def zeta_(self):
+        """convexity adjustment
+
+        Returns:
+            zeta: convex. adj
+        """
         zeta_p = (
             self.alpha_p
             * gamma(-self.beta_p)
@@ -54,7 +85,18 @@ class TemperedStablePricer:
         )
         return -(zeta_p + zeta_m)
 
-    def a1(self, n_vec: tuple, T: float):
+    def a1(
+        self, n_vec: tuple, ttm: float | npt.NDArray[np.float64]
+    ) -> float | npt.NDArray[np.float64]:
+        """a1 coef in the paper
+
+        Args:
+            n_vec (tuple): n_i
+            ttm (float): maturity
+
+        Returns:
+            float: a1 coef
+        """
         n1, n2, n3, n4 = n_vec
         # taylor like term
         taylor = (-1) ** (n1 + n2 + n3) / (
@@ -76,11 +118,22 @@ class TemperedStablePricer:
                 gamma(-self.beta_p * n2)
             )
         # time term
-        at_term = (self.ap * T) ** n2 * (self.am * T) ** n3
+        at_term = (self.ap * ttm) ** n2 * (self.am * ttm) ** n3
 
         return taylor * pochhamer_symb * gamma_term * at_term
 
-    def a2(self, n_vec: tuple, T: float):
+    def a2(
+        self, n_vec: tuple, ttm: float | npt.NDArray[np.float64]
+    ) -> float | npt.NDArray[np.float64]:
+        """a2 coef in the paper
+
+        Args:
+            n_vec (tuple): n_i
+            ttm (float): maturity
+
+        Returns:
+            float: a1 coef
+        """
         n1, n2, n3, n4 = n_vec
         # taylor like term
         taylor = (-1) ** (n1 + n2 + n3 + n4) / (
@@ -96,11 +149,22 @@ class TemperedStablePricer:
                 gamma(-self.beta_p * n2) * gamma(-self.beta_m * n3)
             )
         # time term
-        at_term = (self.ap * T) ** n2 * (self.am * T) ** n3
+        at_term = (self.ap * ttm) ** n2 * (self.am * ttm) ** n3
 
         return taylor * pochhamer_symb * gamma_term * at_term
 
-    def a3(self, n_vec: tuple, T: float):
+    def a3(
+        self, n_vec: tuple, ttm: float | npt.NDArray[np.float64]
+    ) -> float | npt.NDArray[np.float64]:
+        """a3 coef in the paper
+
+        Args:
+            n_vec (tuple): n_i
+            ttm (float): maturity
+
+        Returns:
+            float: a1 coef
+        """
         n1, n2, n3 = n_vec
         # taylor like term
         taylor = -((-1) ** (n1 + n2)) / (factorial(n1) * factorial(n2))
@@ -114,39 +178,40 @@ class TemperedStablePricer:
                 gamma(-self.beta_m * n2)
             )
         # time term
-        at_term = (self.ap * T) ** n1 * (self.am * T) ** n2
+        at_term = (self.ap * ttm) ** n1 * (self.am * ttm) ** n2
         return taylor * pochhamer_symb * gamma_term * at_term
 
     def get_nvecs(self, N: int, rep: int):
-        iterN = list(iter.product(range(N), repeat=rep))
-        return iterN
+        itN = list(it.product(range(N), repeat=rep))
+        return itN
 
-    def price(self, S0: float, K: float, r: float, q: float, T: float, N: int = 15):
-        # mettre k en array[None,:] et T en array [:,None]
-        k = np.log(S0 / K) + (r - q + self.zeta) * T
-        self.display_params(k)
+    def price(self, S0: float, K: float, r: float, q: float, ttm: float, N: int = 15):
+        # mettre k en array[None,:] et ttm en array [:,None]
+        k = np.log(S0 / K) + (r - q + self.zeta) * ttm
+        # self.display_params(k)
+        print(N)
         # series
-        serie1 = self.serie1(k, T, N)
-        serie2 = self.serie2(k, T, N)
-        constant_term = np.exp(k - self.zeta * T) - 1
+        serie1 = self.serie1(k, ttm, N)
+        serie2 = self.serie2(k, ttm, N)
+        constant_term = np.exp(k - self.zeta * ttm) - 1
         # factors
-        factor_serie = np.exp(self.gamma * T)
-        factor = K * np.exp(-r * T)
+        factor_serie = np.exp(self.gamma * ttm)
+        factor = K * np.exp(-r * ttm)
         call_price = factor * (constant_term + factor_serie * (serie1 + serie2))
         return call_price
 
-    def serie1(self, k: float, T: float, N: int):
+    def serie1(self, k: float, ttm: float, N: int):
         nvecs = self.get_nvecs(N, 4)
         serie = 0
         for nvec in nvecs:
             n1, n2, n3, n4 = nvec
             term1 = (
-                self.a1(nvec, T)
+                self.a1(nvec, ttm)
                 * self.ulambda**n1
                 * (-k) ** (n1 - self.beta_p * n2 - self.beta_m * n3 + n4)
             )
             term2 = (
-                self.a2(nvec, T)
+                self.a2(nvec, ttm)
                 * self.ulambda ** (1 + n1 + self.beta_p * n2 + self.beta_m * n3)
                 * (-k) ** (1 + n1 + n4)
             )
@@ -154,12 +219,12 @@ class TemperedStablePricer:
             serie += (term1 + term2) * exp_term
         return serie
 
-    def serie2(self, k: float, T: float, N: int):
+    def serie2(self, k: float, ttm: float, N: int):
         nvecs = self.get_nvecs(N, 3)
         serie = 0
         for nvec in nvecs:
             n1, n2, n3 = nvec
-            term1 = self.a3(nvec, T) * self.ulambda ** (
+            term1 = self.a3(nvec, ttm) * self.ulambda ** (
                 self.beta_p * n1 + self.beta_m * n2 - n3
             )
             exp_term = np.exp(k) * (self.lambda_p - 1) ** n3 - self.lambda_p**n3
---
+++ test_multi_strikes_ttm.py
@@ -0,0 +1,40 @@
+"""importing modules"""
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from coefs_ts_series import TemperedStablePricer
+
+
+def main():
+    """
+    Test of TS pricing
+    """
+    ts_params = dict(
+        alpha_p=0.44,
+        beta_p=0.1 + np.exp(1) / 10,
+        lambda_p=1.4,
+        alpha_m=0.35,
+        beta_m=0.5 - np.pi / 100,
+        lambda_m=0.4,
+    )
+    strikes = np.arange(1.2, 20, 0.2)[None, :]
+    times = np.arange(1, 2, 0.1)[:, None]
+    option_params = dict(S0=1, K=strikes, r=0.02, q=0.05, ttm=times)
+    ts_pricer = TemperedStablePricer(**ts_params)
+    t0 = time.time()
+    prices = ts_pricer.price(**option_params, N=25)
+    t = time.time()
+    print(prices.shape, f"Time: {t-t0}")
+    for i in range(prices.shape[0]):
+        plt.plot(strikes[0], prices[i], label=f"T={times[i]}")
+    plt.legend()
+    plt.grid()
+    plt.xlabel("Strike")
+    plt.ylabel("Call price")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
