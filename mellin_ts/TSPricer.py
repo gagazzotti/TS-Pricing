a39c42aa2eefec39f7f8dc12d44595bcebbe8453
GaetanoCodes <gaetano.agazzotti@hotmail.fr>
2024-10-05 15:05:43 +0200

    vectorized with time

--- mellin_ts/TSPricer.py
+++ mellin_ts/TSPricer.py
@@ -119,7 +119,7 @@ class TemperedStablePricer:
             * self.ulambda**n1
             * (-k) ** (n1 - self.beta_p * n2 - self.beta_m * n3 + n4)
         )
-        print(term1.shape)
+        # print(term1.shape)
         term2 = (
             self.a2(N, ttm)
             * self.ulambda ** (1 + n1 + self.beta_p * n2 + self.beta_m * n3)
@@ -132,10 +132,10 @@ class TemperedStablePricer:
 
     def a1(self, N: int, ttm):
         # ttm = np.array(ttm)[None, None, None, None, :]
-        n1 = np.arange(N)[:, None, None, None, None]
-        n2 = np.arange(N)[None, :, None, None, None]
-        n3 = np.arange(N)[None, None, :, None, None]
-        n4 = np.arange(N)[None, None, None, :, None]
+        n1 = np.arange(N)[:, None, None, None]
+        n2 = np.arange(N)[None, :, None, None]
+        n3 = np.arange(N)[None, None, :, None]
+        n4 = np.arange(N)[None, None, None, :]
         taylor = (-1) ** (n1 + n2 + n3) / (
             factorial(n1) * factorial(n2) * factorial(n3) * factorial(n4)
         )
@@ -145,7 +145,7 @@ class TemperedStablePricer:
             / gamma(1 + self.beta_p * n2)
         )
         at_term = (self.ap * ttm) ** n2 * (self.am * ttm) ** n3
-        print(at_term.shape)
+        # print(at_term.shape)
         gamma_term = gamma(-n1 + self.beta_p * n2 + self.beta_m * n3 - n4) / (
             gamma(-self.beta_p * n2)
         )
@@ -154,9 +154,9 @@ class TemperedStablePricer:
             * (self.beta_p / (self.beta_p + self.beta_m))
         )[:, 0, 0, :]
         a1 = pochhamer_symb * at_term * gamma_term * taylor
-        print(a1.sum())
+        # print(a1.sum())
 
-        print("###########################")
+        # print("###########################")
         return a1
 
     def a2(self, N: int, ttm):
@@ -178,14 +178,126 @@ class TemperedStablePricer:
         return a2
 
     def price(self, S0: float, K: float, r: float, q: float, ttm: float, N: int = 5):
-        # ttm = np.array(ttm)[None, None, None, None, :]
         k = np.log(S0 / K) + (r - q + self.zeta) * ttm
-        # print(k)
         serie1 = self.serie1(k, ttm, N)
         serie2 = self.serie2(k, ttm, N)
-        # print(serie1, serie2)
         constant_term = np.exp(k - self.zeta * ttm) - 1
         # factors
+        factor_serie = np.exp(self.gamma * ttm)
+        factor = K * np.exp(-r * ttm)
+        call_price = factor * (constant_term + factor_serie * (serie1 + serie2))
+        return call_price
+
+    #####################
+    #### VECT ###########
+    #####################
+
+    def a1_vect(self, N: int, ttm):
+        ttm_vec = np.array(ttm)[None, None, None, None, :]
+        n1 = np.arange(N)[:, None, None, None, None]
+        n2 = np.arange(N)[None, :, None, None, None]
+        n3 = np.arange(N)[None, None, :, None, None]
+        n4 = np.arange(N)[None, None, None, :, None]
+        taylor = (-1) ** (n1 + n2 + n3) / (
+            factorial(n1) * factorial(n2) * factorial(n3) * factorial(n4)
+        )
+        pochhamer_symb = (
+            poch(-self.beta_m * n3, n1)
+            * poch(n1 - self.beta_p * n2 - self.beta_m * n3, n4)
+            / gamma(1 + self.beta_p * n2)
+        )
+        at_term = (self.ap * ttm_vec) ** n2 * (self.am * ttm_vec) ** n3
+        # print(at_term.shape)
+        gamma_term = gamma(-n1 + self.beta_p * n2 + self.beta_m * n3 - n4) / (
+            gamma(-self.beta_p * n2)
+        )
+        gamma_term[:, 0, 0, :, :] = (
+            ((-1) ** (1 + n1 + n4) / factorial(n1 + n4))
+            * (self.beta_p / (self.beta_p + self.beta_m))
+        )[:, 0, 0, :, :]
+        a1 = pochhamer_symb * at_term * gamma_term * taylor
+        # print(a1.sum())
+        # print(a1.shape)
+        print("###########################")
+        return a1
+
+    def a2_vect(self, N: int, ttm):
+        ttm_vec = np.array(ttm)[None, None, None, None, :]
+        n1 = np.arange(N)[:, None, None, None, None]
+        n2 = np.arange(N)[None, :, None, None, None]
+        n3 = np.arange(N)[None, None, :, None, None]
+        n4 = np.arange(N)[None, None, None, :, None]
+        taylor = (-1) ** (n1 + n2 + n3 + n4) / (
+            factorial(n1) * factorial(n2) * factorial(n3) * factorial(n4)
+        )
+        pochhamer_symb = poch(1 + self.beta_p * n2, n1) / (-1 - n1 - n4)
+        gamma_term = gamma(-1 - n1 - self.beta_p * n2 - self.beta_m * n3) / (
+            gamma(-self.beta_p * n2) * gamma(-self.beta_m * n3)
+        )
+        gamma_term[:, 0, 0, :] = 0
+        # time term
+        at_term = (self.ap * ttm_vec) ** n2 * (self.am * ttm_vec) ** n3
+        a2 = taylor * pochhamer_symb * gamma_term * at_term
+        return a2
+
+    def serie1_vect(self, k: float, ttm: float, N: int):
+        ttm_vec = np.array(ttm)[None, None, None, None, :]
+        n1 = np.arange(N)[:, None, None, None, None]
+        n2 = np.arange(N)[None, :, None, None, None]
+        n3 = np.arange(N)[None, None, :, None, None]
+        n4 = np.arange(N)[None, None, None, :, None]
+
+        term1 = (
+            self.a1_vect(N, ttm)
+            * self.ulambda**n1
+            * (-k) ** (n1 - self.beta_p * n2 - self.beta_m * n3 + n4)
+        )
+        term2 = (
+            self.a2_vect(N, ttm)
+            * self.ulambda ** (1 + n1 + self.beta_p * n2 + self.beta_m * n3)
+            * (-k) ** (1 + n1 + n4)
+        )
+        exp_term = np.exp(k) * (self.lambda_p - 1) ** n4 - self.lambda_p**n4
+        serie = (term1 + term2) * exp_term
+        serie = serie.sum(axis=(0, 1, 2, 3))
+        return serie
+
+    def serie2_vect(self, k: float, ttm: float, N: int):
+        k_vec = k[0]
+        print(k_vec.shape, k.shape)
+        ttm_vec = np.array(ttm)[None, None, None, :]
+        n1 = np.arange(N)[:, None, None, None]
+        n2 = np.arange(N)[None, :, None, None]
+        n3 = np.arange(N)[None, None, :, None]
+        taylor_term = -((-1) ** (n1 + n2)) / (factorial(n1) * factorial(n2))
+        gamma_term = np.zeros((N, N, N))
+        gamma_term = gamma(-self.beta_p * n1 - self.beta_m * n2 + n3) / (
+            gamma(1 - self.beta_p * n1 + n3) * gamma(-self.beta_m * n2)
+        )
+        gamma_term[0, 0, 0] = self.beta_m / (self.beta_m + self.beta_p)
+        ulambda_term = self.ulambda ** (self.beta_p * n1 + self.beta_m * n2 - n3)
+        exp_term = np.exp(k_vec) * (self.lambda_p - 1) ** n3 - self.lambda_p**n3
+        at = (self.ap * ttm_vec) ** n1 * (self.am * ttm_vec) ** n2
+        serie = (taylor_term * gamma_term * exp_term * at * ulambda_term).sum(
+            axis=(0, 1, 2)
+        )
+        return serie
+
+    def price_vect(
+        self,
+        S0: float,
+        K: float,
+        r: float,
+        q: float,
+        ttm: npt.NDArray[np.float64],
+        N: int = 5,
+    ):
+        ttm_vec = np.array(ttm)[None, None, None, None, :]
+        k = np.log(S0 / K) + (r - q + self.zeta) * ttm_vec
+        serie1 = self.serie1_vect(k, ttm, N)
+        serie2 = self.serie2_vect(k, ttm, N)
+        constant_term = np.exp(k - self.zeta * ttm) - 1
+        # # factors
         factor_serie = np.exp(self.gamma * ttm)
         factor = K * np.exp(-r * ttm)
         call_price = factor * (constant_term + factor_serie * (serie1 + serie2))
--- tests/test_vect.py
+++ tests/test_vect.py
@@ -17,31 +17,34 @@ plt.style.use(["science"])
 def main():
     """Testing the convergence of the pricer"""
     ts_params = dict(
-        alpha_p=0.4,
-        beta_p=0.4 + np.exp(1) / 10,
+        alpha_p=0.44,
+        beta_p=0.1 + np.exp(1) / 10,
         lambda_p=1.4,
-        alpha_m=0.05,
+        alpha_m=0.35,
         beta_m=0.5 - np.pi / 100,
         lambda_m=0.4,
     )
-    strike = 1.8
-    ttm = np.array([2])
-    option_params = dict(S0=1, K=strike, r=0.02, q=0.05, ttm=ttm)
-    ts_pricer = TemperedStablePricer(**ts_params)
     N = 20
-    print("N:", N)
-    # t0 = time()
-    # price = ts_pricer.price(**option_params, N=N)
-    # print("Time non vect", time() - t0)
+    strike = 1.1
+    ttm = np.arange(0.5, 1.5, 1e-2)
     t0 = time()
-    price = ts_pricer.price(**option_params, N=N)
+    prices = []
+    for t in ttm:
+        option_params = dict(S0=1, K=strike, r=0.02, q=0.05, ttm=t)
+        ts_pricer = TemperedStablePricer(**ts_params)
+        price = ts_pricer.price(**option_params, N=N)
+        prices.append(price)
     print("Time", time() - t0)
 
-    # t0 = time()
-    # price_vect = ts_pricer.price(**option_params, N=N)
-    # print("Time vect", time() - t0)
-    # print("Error:", price_vect - price)
-    # print("Diff price:", price_vect - price)
+    option_params = dict(S0=1, K=strike, r=0.02, q=0.05, ttm=ttm)
+    t0 = time()
+    price_vect = ts_pricer.price_vect(**option_params, N=N)
+    print(
+        "Time",
+        time() - t0,
+        "Diff prices L1",
+        np.abs(np.array(prices) - price_vect[0, 0, 0, 0]).sum(),
+    )
 
 
 if __name__ == "__main__":
